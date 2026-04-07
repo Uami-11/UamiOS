d00c5fb19fdaab22a434e5576deb49c833ed6287
Uami-11 <nirwan.m.9618@gmail.com>
2026-04-07 09:10:03 +0545

    add: implement printf for the bootloader

--- src/bootloader/stage2/stdint.h
+++ src/bootloader/stage2/stdint.h
@@ -4,7 +4,12 @@ typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef signed short int16_t;
 typedef unsigned short uint16_t;
-typedef signed long int32_t;
-typedef unsigned long uint32_t;
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
+typedef signed long int int32_t;
+typedef unsigned long int uint32_t;
+typedef signed long long int int64_t;
+typedef unsigned long long int uint64_t;
+
+typedef uint8_t bool;
+
+#define false 0
+#define true 1
