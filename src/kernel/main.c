09ea918b4b7e76924351d579764896fb12ca3753
Uami-11 <nirwan.m.9618@gmail.com>
2026-04-12 13:56:10 +0545

    add: implement Global Descriptor Table and Interrupt Descriptor Table

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,5 +1,6 @@
 #include "memory.h"
 #include "stdio.h"
+#include <hal/hal.h>
 #include <stdint.h>
 
 extern uint8_t __bss_start;
@@ -8,7 +9,10 @@ extern uint8_t __end;
 void __attribute__((section(".entry"))) start(uint16_t bootDrive) {
 	memset(&__bss_start, 0, (&__end) - (&__bss_start));
 
+	HAL_Initialize();
+
 	clrscr();
+
 	printf("\r\n"
 		   "@@@  @@@   @@@@@@   @@@@@@@@@@   @@@   @@@@@@    @@@@@@   \r\n"
 		   "@@@  @@@  @@@@@@@@  @@@@@@@@@@@  @@@  @@@@@@@@  @@@@@@@   \r\n"
@@ -21,8 +25,6 @@ void __attribute__((section(".entry"))) start(uint16_t bootDrive) {
 		   "::::: ::  ::   :::  :::     ::    ::  ::::: ::  :::: ::   \r\n"
 		   " : :  :    :   : :   :      :    :     : :  :   :: : :    \r\n"
 		   "\r\n");
-	printf("Hello world from kernel!!!\n");
-
 end:
 	for (;;)
 		;
